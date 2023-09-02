466bf155ceefd19d65043b8e54e2af4f81327c24
Irors <81914651+Irors@users.noreply.github.com>
2023-09-03 00:15:28 +0500

    Add files via upload

--- Leaderboard.py
+++ Leaderboard.py
@@ -3,8 +3,17 @@ import openpyxl
 
 headers = {
     'authority': 'quests.mystenlabs.com',
+    'accept': '*/*',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
     'content-type': 'application/json',
-    'referer': 'https://quests.mystenlabs.com/'
+    'referer': 'https://quests.mystenlabs.com/',
+    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
 }
 
 
@@ -26,26 +35,35 @@ for wallet in wallets:
     }
 
     response = requests.get('https://quests.mystenlabs.com/api/trpc/user', params=params, headers=headers)
-    address = response.json()[0]['result']['data']['address']
-    bot = response.json()[0]['result']['data']['bot']
-    score = response.json()[0]['result']['data']['score']
-    rank = response.json()[0]['result']['data']['rank']
-    protocol = response.json()[0]['result']['data']['metadata']['appsUsed']
+    try:
+        bot = response.json()[0]['result']['data']['bot']
+        score = response.json()[0]['result']['data']['score']
+        rank = response.json()[0]['result']['data']['rank']
+        protocol = response.json()[0]['result']['data']['metadata']['appsUsed']
+        SUI_TVL = response.json()[0]['result']['data']['metadata']['SUI_TVL']
+        reward = response.json()[0]['result']['data']['reward']
 
 
-    ''''Работа с excel'''
+        ''''Работа с excel'''
 
-    sheet['A1'] = 'address'
-    sheet['B1'] = 'bot'
-    sheet['C1'] = 'score'
-    sheet['D1'] = 'rank'
-    sheet['E1'] = 'protocol'
+        sheet['A1'] = 'address'
+        sheet['B1'] = 'bot'
+        sheet['C1'] = 'score'
+        sheet['D1'] = 'rank'
+        sheet['E1'] = 'protocol'
+        sheet['F1'] = 'SUI_TVL'
+        sheet['G1'] = 'reward'
 
-    sheet[f'A{cou}'] = address
-    sheet[f'B{cou}'] = bot
-    sheet[f'C{cou}'] = score
-    sheet[f'D{cou}'] = rank
-    sheet[f'E{cou}'] = len(protocol)
-    cou += 1
 
-    workbook.save('Leaderboard_stat.xlsx')
+        sheet[f'A{cou}'] = wallet
+        sheet[f'B{cou}'] = bot
+        sheet[f'C{cou}'] = score
+        sheet[f'D{cou}'] = rank
+        sheet[f'E{cou}'] = len(protocol)
+        sheet[f'F{cou}'] = SUI_TVL
+        sheet[f'G{cou}'] = reward
+        cou += 1
+
+        workbook.save('Leaderboard_stat.xlsx')
+    except:
+        pass
